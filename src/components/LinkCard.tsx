b56ef1d4f7049ba269af14c4e59bff9588c56082
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-25 19:11:01 -0300

    style: ajusta código com prettier

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -11,13 +11,22 @@ export function LinkCard({ href, title, image }: LinkCardProps) {
       <a
         href={href}
         target="_blank"
-        className="flex w-full p-1 rounded-md hover:scale-105 transition-all border border-gray-300 mb-3" rel="noreferrer"
+        className="flex w-full p-1 rounded-md hover:scale-105 transition-all border border-gray-300 mb-3 bg-gray-100"
+        rel="noreferrer"
       >
         <div className="flex items-center text-center w-full">
           {image && (
-            <Image className="rounded-sm" src={image} alt={title} width="40" height="40"/>
-          )}  
-          <h2 className="font-semibold w-full text-center">{title}</h2>
+            <Image
+              className="rounded-sm"
+              src={image}
+              alt={title}
+              width="40"
+              height="40"
+            />
+          )}
+          <h2 className="font-semibold w-full text-center text-gray-700">
+            {title}
+          </h2>
         </div>
       </a>
     );
