313e73607863f191823c4e4e73de5f1b1905bf70
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-25 17:24:32 -0300

    perf: ajusta host da url de imagens

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["avatars.githubusercontent.com", "paulomoutinho.vercel.app", "pbs.twimg.com", "media.licdn.com"]
+  }
 }
 
 module.exports = nextConfig
