029032d266ae989a33181d61ca0face67f6de6f3
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-25 12:13:41 -0300

    feat: inicia dev com tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <p>Hello, Next.</p>
-      </header>
+      <h1 className="text-3xl font-bold">Hello, World!!</h1>
     </>
   )
 }
