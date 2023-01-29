609fff2e0e5c393af5c062653b665dacefb7e529
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-29 19:09:15 -0300

    feat: adiciona ícones com rotas para redes sociais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import { LinkCard } from '@/components/LinkCard';
 import data from "../../data.json"
+import { InstagramIcon, LinkedinIcon } from '@/components/SocialIcons';
 
 export default function Home() {
   return (
@@ -24,6 +26,28 @@ export default function Home() {
       {data.links.map((link, index) => (
         <LinkCard key={index} {...link}/>
       ))}
+      <div className="flex ite gap-4 mt-6">
+        {data.socials.map((social, index) => {
+          if (social.title.includes("Instagram")) {
+            return (
+              <Link href={social.href} passHref target="_blank" key={index} 
+                className="hover:scale-105 transition-all"
+              >
+                <InstagramIcon />
+              </Link>
+            )
+          }
+          if (social.title.includes("LinkedIn")) {
+            return (
+              <Link href={social.href} passHref target="_blank" key={index}
+                className="hover:scale-105 transition-all"
+              >
+                <LinkedinIcon />
+              </Link>
+            )
+          }
+        })}
+      </div>
     </div>
   );
 }
