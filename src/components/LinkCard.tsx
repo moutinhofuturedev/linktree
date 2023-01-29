6dd9e444f259858bbc854d70d7c1f57f2646fa8f
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-29 19:07:49 -0300

    style: formata código

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -6,7 +6,18 @@ export function LinkCard({ href, title, image }: LinkCardProps) {
       <a
         href={href}
         target="_blank"
-        className="flex w-full p-1 rounded-md hover:scale-105 transition-all border border-gray-300 mb-3 bg-gray-100"
+        className="flex 
+        w-80 
+        md:w-[480px]
+        lg:w-[520px] 
+        p-1 
+        rounded-md 
+        hover:scale-105 
+        transition-all 
+        border 
+        border-gray-300 
+        mb-3 
+        bg-gray-100"
         rel="noreferrer"
       >
         <div className="flex items-center text-center w-full h-10">
